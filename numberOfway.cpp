28753d533cd4a2622346218d011ebbc539a8c1a4
Pooja Prajapati <99605021+Gun2902@users.noreply.github.com>
2024-01-05 00:02:08 +0530

    Update numberOfway.cpp

--- numberOfway.cpp
+++ numberOfway.cpp
@@ -15,10 +15,10 @@ public:
 
         vector<vector<int>> dp(k + 1, vector<int>(MAX_POSITION, 0));
 
-        // Base case: At step 0, there's only one way to be at the starting position
+        // Base case At step 0, there's only one way to be at the starting position
         dp[0][startPos] = 1;
 
-        // Dynamic Programming to calculate the number of ways
+        
         for (int step = 1; step <= k; ++step)
         {
             for (int pos = 0; pos < MAX_POSITION; ++pos)
@@ -30,7 +30,7 @@ public:
             }
         }
 
-        // The result is the number of ways to reach the endPos after k steps
+       
         return dp[k][endPos];
     }
 };
@@ -39,7 +39,7 @@ int main()
 {
     Solution solution;
 
-    // Input
+ 
     int startPos, endPos, k;
     cout << "Enter the starting position: ";
     cin >> startPos;
@@ -48,7 +48,7 @@ int main()
     cout << "Enter the number of steps: ";
     cin >> k;
 
-    // Calculate and output the result
+   
     int result = solution.countWays(startPos, endPos, k);
     cout << "Number of Ways: " << result << endl;
 
